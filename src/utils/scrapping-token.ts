d7575f27ea6e87785337ac888e01253a0ef4309b
Diego Ramos <diegorbrito9@gmail.com>
2024-10-09 09:30:06 -0300

    feat: scrapping lambda

--- src/utils/scrapping-token.ts
+++ src/utils/scrapping-token.ts
@@ -1,37 +1,54 @@
-import puppeteer from 'puppeteer';
+import chromium from 'chrome-aws-lambda';
+import puppeteer, { Browser } from 'puppeteer-core';
 
 export async function getBearerToken() {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
+  let browser: Browser | null = null;
 
-  await page.goto('https://en.freqtek.com/', { waitUntil: 'networkidle0' });
-  await page.waitForSelector('#verifyEn');
-  await page.waitForSelector('#one');
-  await page.waitForSelector('#login');
+  try {
+    browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
 
-  const emailInput = await page.$('[name="Username"]');
-  const passwordInput = await page.$('[name="Password"]');
+    const page = await browser.newPage();
 
-  if (!emailInput || !passwordInput) {
-    throw new Error('Email or Password input not found');
+    await page.goto('https://en.freqtek.com/', { waitUntil: 'networkidle0' });
+
+    await page.waitForSelector('#verifyEn');
+    await page.waitForSelector('#one');
+    await page.waitForSelector('#login');
+
+    const emailInput = await page.$('[name="Username"]');
+    const passwordInput = await page.$('[name="Password"]');
+
+    if (!emailInput || !passwordInput) {
+      throw new Error('Email or Password input not found');
+    }
+
+    await emailInput.type('lab3d.fortaleza@gmail.com', { delay: 10 });
+    await passwordInput.type('Bucal1234', { delay: 10 });
+
+    await page.evaluate(() => {
+      const loginButton = document.querySelector('#login') as HTMLButtonElement;
+      loginButton.click();
+    });
+
+    await page.waitForNavigation();
+    await page.waitForSelector('#app');
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    const token = await page.evaluate(() => sessionStorage.getItem('token'));
+
+    console.log('Scraping finalizado com sucesso.');
+
+    return token;
+  } catch (error) {
+    console.error('Erro ao executar o scraping:', error);
+    throw error;
+  } finally {
+    if (browser !== null) await browser.close();
   }
-
-  await emailInput.type('lab3d.fortaleza@gmail.com', { delay: 10 });
-  await passwordInput.type('Bucal1234', { delay: 10 });
-
-  await page.evaluate(() => {
-    const loginButton = document.querySelector('#login') as HTMLButtonElement;
-    loginButton.click();
-  });
-
-  await page.waitForNavigation();
-  await page.waitForSelector('#app');
-  await new Promise(resolve => setTimeout(resolve, 1000));
-
-  const token = await page.evaluate(() => sessionStorage.getItem('token'));
-  await browser.close();
-
-  console.log('finish scrapping');
-
-  return token;
 }
